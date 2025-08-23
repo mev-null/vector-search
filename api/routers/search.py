b1fc59af04933165dd8984fbcda54599d52a62c7
Nemoto Mizuki <n.mizuki.040618@gmail.com>
2025-08-23 21:03:34 +0900

    Update search.py
    rewrite comments

--- api/routers/search.py
+++ api/routers/search.py
@@ -1,4 +1,3 @@
-#api/routers/search.py
 from fastapi import APIRouter, Depends
 from typing import List
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -14,15 +13,16 @@ _model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 @router.post("/search", response_model=List[schemas.SearchResultItem])
 async def search(request_body: schemas.SearchQuery ,db: AsyncSession=Depends(get_async_db)):
   """
-  入力: ユーザーからのJSONデータ（{"query": "..."}）。
-  処理:
-    queryをsentence-transformersでベクトル化。
-    embeddingとlimitをcrud.pyの検索関数に渡す。
-    crud.pyがSQLAlchemy経由でpgvectorに検索クエリを送信。
-    データベースが類似データ（title, url, date, embeddingなど）を返す。
-    SQLAlchemyがmodelsオブジェクトに変換し、Pydanticのschemasがtitle, url, dateをJSONに整形。
-  出力: JSON形式の検索結果（{"results": [...]}）。
-  """
+  ユーザーからの検索クエリを元に、データベースから類似画像を検索
+
+  Args:
+    -request_body (schemas.SearchQuery): `query` (検索テキスト) と `limit`(取得件数) を含むリクエストボディ。
+    -db (AsyncSession): データベースへの非同期セッション。
+
+  Returns:
+    List[schemas.SearchResultItem]: 検索結果のリスト。各項目は`title`,`date`, `url`を含むJSONオブジェクトに整形
+
+"""
   query_embedding = _model.encode(request_body.query).tolist()
   results = await crud.search_vector(db, query_embedding, request_body.limit)
 
